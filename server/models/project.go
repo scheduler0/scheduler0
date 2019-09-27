f045a5b337589b1c6747cda93db1e7905c24b03f
Nwaokocha Victor <nvonweb@outlook.com>
2019-09-26 22:22:45 -0500

    Refactor get all and delete one methods

--- server/models/project.go
+++ server/models/project.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/go-pg/pg"
 	"github.com/segmentio/ksuid"
 	"reflect"
@@ -26,9 +27,34 @@ func (p *Project) CreateOne() (string, error) {
 	})
 	defer db.Close()
 
+	if len(p.Name) < 1 {
+		err := errors.New("name field is required")
+		return "", err
+	}
+
+	if len(p.Description) < 1 {
+		err := errors.New("description field is required")
+		return "", err
+	}
+
+	var projectWithName = Project{}
+	data, err := projectWithName.GetAll("name LIKE ?", p.Name)
+
+	vd := reflect.ValueOf(data)
+	projectsWithName := make([]Project, vd.Len())
+
+	for i := 0; i < vd.Len(); i++ {
+		projectsWithName[i] = vd.Index(i).Interface().(Project)
+	}
+
+	if err == nil && len(projectsWithName) > 0 {
+		err := errors.New("projects exits with the same name")
+		return "", err
+	}
+
 	p.ID = ksuid.New().String()
 
-	_, err := db.Model(p).Insert()
+	_, err = db.Model(p).Insert()
 	if err != nil {
 		return "", err
 	}
@@ -36,7 +62,7 @@ func (p *Project) CreateOne() (string, error) {
 	return p.ID, nil
 }
 
-func (p *Project) GetOne() error {
+func (p *Project) GetOne(query string, params interface{}) error {
 	db := pg.Connect(&pg.Options{
 		Addr:     psgc.Addr,
 		User:     psgc.User,
@@ -45,7 +71,7 @@ func (p *Project) GetOne() error {
 	})
 	defer db.Close()
 
-	err := db.Model(p).Where("id = ?", p.ID).Select()
+	err := db.Model(p).Where(query, params).Select()
 	if err != nil {
 		return err
 	}
@@ -53,7 +79,7 @@ func (p *Project) GetOne() error {
 	return nil
 }
 
-func (p *Project) GetAll() (interface{}, error) {
+func (p *Project) GetAll(query string, params interface{}) ([]interface{}, error) {
 	db := pg.Connect(&pg.Options{
 		Addr:     psgc.Addr,
 		User:     psgc.User,
@@ -63,15 +89,18 @@ func (p *Project) GetAll() (interface{}, error) {
 	defer db.Close()
 
 	var projects []Project
-
-	err := db.Model(&projects).Select()
+	err := db.Model(&projects).Where(query, params).Select()
 	if err != nil {
-		return projects, err
+		return []interface{}{}, err
 	}
 
-	vp := reflect.New(reflect.TypeOf(projects))
-	vp.Elem().Set(reflect.ValueOf(projects))
-	return vp.Interface(), nil
+	var results = make([]interface{}, len(projects))
+
+	for i := 0; i < len(projects); i++ {
+		results[i] = projects[i]
+	}
+
+	return results, nil
 }
 
 func (p *Project) UpdateOne() error {
@@ -91,7 +120,7 @@ func (p *Project) UpdateOne() error {
 	return nil
 }
 
-func (p *Project) DeleteOne() error {
+func (p *Project) DeleteOne() (int, error) {
 	db := pg.Connect(&pg.Options{
 		Addr:     psgc.Addr,
 		User:     psgc.User,
@@ -100,12 +129,24 @@ func (p *Project) DeleteOne() error {
 	})
 	defer db.Close()
 
-	_, err := db.Model(p).Where("id = ?", p.ID).Delete()
+	var jobs []Job
+
+	err := db.Model(&jobs).Where("project_id = ?", p.ID).Select()
 	if err != nil {
-		return err
+		return -1, err
 	}
 
-	return nil
+	if len(jobs) > 0 {
+		err = errors.New("cannot delete projects with jobs")
+		return -1, err
+	}
+
+	r, err := db.Model(p).Where("id = ?", p.ID).Delete()
+	if err != nil {
+		return -1, err
+	}
+
+	return r.RowsAffected(), nil
 }
 
 func (p *Project) ToJson() []byte {
