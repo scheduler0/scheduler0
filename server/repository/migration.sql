ffdfb8b3dc5e5e55ae8947b0ceaf0746a1b78f5b
Nwaokocha Victor <nvonweb@outlook.com>
2019-09-27 16:42:01 -0500

    Group migrations

--- server/repository/migration.sql
+++ server/repository/migration.sql
@@ -1,5 +1,6 @@
 SET TIME ZONE 'UTC';
 
+-- Job schema migrations
 ALTER TABLE jobs DROP COLUMN IF EXISTS service_name;
 
 DO $$
@@ -11,10 +12,7 @@ BEGIN
         ELSE
             ALTER TABLE jobs ADD project_id varchar(255);
     END IF;
-END $$;
 
-DO $$
-BEGIN
     IF EXISTS (
         SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
         WHERE TABLE_NAME='jobs' AND COLUMN_NAME='missed_execs')
@@ -22,11 +20,7 @@ BEGIN
         ELSE
             ALTER TABLE jobs ADD missed_execs int;
     END IF;
-END $$;
 
-
-DO $$
-BEGIN
     IF EXISTS (
             SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
             WHERE TABLE_NAME='jobs' AND COLUMN_NAME='callback_url')
@@ -34,15 +28,40 @@ BEGIN
     ELSE
         ALTER TABLE jobs ADD callback_url varchar(255);
     END IF;
-END $$;
 
-DO $$
-BEGIN
     IF EXISTS (
             SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
             WHERE TABLE_NAME='jobs' AND COLUMN_NAME='last_status_code')
     THEN
     ELSE
         ALTER TABLE jobs ADD last_status_code int;
+    END IF;
+
+    IF EXISTS (
+            SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME='jobs' AND COLUMN_NAME='description')
+    THEN
+    ELSE
+        ALTER TABLE jobs ADD description varchar(255);
+    END IF;
+
+    IF EXISTS (
+            SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME='jobs' AND COLUMN_NAME='date_created')
+    THEN
+    ELSE
+        ALTER TABLE jobs ADD date_created timestamp;
+    END IF;
+END $$;
+
+-- Project schema migrations
+DO $$
+BEGIN
+    IF EXISTS (
+            SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME='projects' AND COLUMN_NAME='date_created')
+    THEN
+    ELSE
+        ALTER TABLE projects ADD date_created timestamp;
     END IF;
 END $$;
