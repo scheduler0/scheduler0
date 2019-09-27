ff3d5dd874931ebf178ff5bcbfbca74f5dd8aeb7
Nwaokocha Victor <nvonweb@outlook.com>
2019-09-27 16:39:24 -0500

    Credentials models

---
+++ server/models/credential_model_test.go
@@ -0,0 +1,17 @@
+package models
+
+import "testing"
+
+var credential Credential
+
+func TestCredential_CreateOne(t *testing.T) {
+	t.Log("Do not create credential without keys")
+	{
+
+	}
+
+	t.Log("Create credential with valid keys")
+	{
+
+	}
+}
