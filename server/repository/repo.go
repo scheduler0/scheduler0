683028d0a29b436bda13edea4add73c0929e67a0
Nwaokocha Victor <nvonweb@outlook.com>
2019-09-27 16:44:56 -0500

    Create table for users and credentials

--- server/repository/repo.go
+++ server/repository/repo.go
@@ -5,11 +5,9 @@ package repository
 import (
 	"cron-server/server/misc"
 	"cron-server/server/models"
-	"fmt"
 	"github.com/go-pg/pg"
 	"github.com/go-pg/pg/orm"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 )
@@ -28,22 +26,24 @@ func Setup() {
 	var runMigrations = func() {
 		pwd, err := os.Getwd()
 		misc.CheckErr(err)
+
 		absPath, err := filepath.Abs(pwd + "/repository/migration.sql")
 		misc.CheckErr(err)
 
-		fmt.Println(absPath)
 		sql, err := ioutil.ReadFile(absPath)
 		misc.CheckErr(err)
 
 		if len(sql) > 0 {
-			log.Println("Running Migration :: ")
-			log.Println(string(sql))
 			_, err = db.Exec(string(sql))
 			misc.CheckErr(err)
 		}
 	}
 
-	for _, model := range []interface{}{(*models.Job)(nil), (*models.Project)(nil)} {
+	for _, model := range []interface{}{
+		(*models.Job)(nil),
+		(*models.Project)(nil),
+		(*models.Credential)(nil),
+	} {
 		err := db.CreateTable(model, &orm.CreateTableOptions{IfNotExists: true})
 		misc.CheckErr(err)
 		runMigrations()
