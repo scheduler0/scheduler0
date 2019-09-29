acda2488125c6fa82e5455caeccf36fbf35d9703
Nwaokocha Victor <nvonweb@outlook.com>
2019-09-29 01:04:43 -0500

    Truncate db function

---
+++ server/testutils/testutils.go
@@ -0,0 +1,25 @@
+package testutils
+
+import (
+	"cron-server/server/misc"
+	"github.com/go-pg/pg"
+)
+
+var psgc = misc.GetPostgresCredentials()
+
+func TruncateDBBeforeTest() {
+	db := pg.Connect(&pg.Options{
+		Addr:     psgc.Addr,
+		User:     psgc.User,
+		Password: psgc.Password,
+		Database: psgc.Database,
+	})
+	defer db.Close()
+
+	truncateQuery := "" +
+		"TRUNCATE TABLE jobs;" +
+		"TRUNCATE TABLE projects;"
+
+	_, err := db.Exec(truncateQuery)
+	misc.CheckErr(err)
+}
