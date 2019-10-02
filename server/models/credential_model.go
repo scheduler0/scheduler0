f405e845b3e27b45ad10de1bd44937fb6eeb9392
Nwaokocha Victor <nvonweb@outlook.com>
2019-10-02 05:07:46 -0500

    Add context argument to job model

--- server/models/credential_model.go
+++ server/models/credential_model.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"context"
+	"cron-server/server/repository"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"github.com/go-pg/pg"
@@ -11,10 +15,7 @@ import (
 type Credential struct {
 	ID                      string    `json:"id"`
 	ApiKey                  string    `json:"api_key"`
-	ApiSecret               string    `json:"api_secret"`
-	PublicKey               string    `json:"public_key"`
 	HTTPReferrerRestriction string    `json:"http_referrer_restriction"`
-	IPAddressRestriction    string    `json:"ip_address_restriction"`
 	DateCreated             time.Time `json:"date_created"`
 }
 
@@ -22,39 +23,127 @@ func (c *Credential) SetId(id string) {
 	c.ID = id
 }
 
-func (c *Credential) CreateOne() (string, error) {
-	if len(c.ApiKey) < 1 {
-		return "", errors.New("api key is required")
-	}
-
-	if len(c.ApiSecret) < 1 {
-		return "", errors.New("api secret is required")
-	}
-
-	if len(c.PublicKey) < 1 {
-		return "", errors.New("public key is required")
-	}
-
-	if len(c.HTTPReferrerRestriction) < 1 && len(c.IPAddressRestriction) < 1 {
+func (c *Credential) CreateOne(pool *repository.Pool, ctx context.Context) (string, error) {
+	if len(c.HTTPReferrerRestriction) < 1 {
 		return "", errors.New("credential should have at least one restriction set")
 	}
 
 	c.DateCreated = time.Now().UTC()
 	c.ID = ksuid.New().String()
+	hash := sha256.New()
+	hash.Write([]byte(c.ID))
+	c.ApiKey = hex.EncodeToString(hash.Sum(nil))
 
-	db := pg.Connect(&pg.Options{
-		Addr:     psgc.Addr,
-		User:     psgc.User,
-		Password: psgc.Password,
-		Database: psgc.Database,
-	})
-	defer db.Close()
+	conn, err := pool.Acquire()
+	defer pool.Release(conn)
+	if err != nil {
+		return "", err
+	}
+	db := conn.(*pg.DB)
 
 	if _, err := db.Model(c).Insert(); err != nil {
 		return "", err
 	} else {
 		return c.ID, nil
 	}
+}
+
+func (c *Credential) GetOne(pool *repository.Pool, ctx context.Context, query string, params interface{}) error {
+	conn, err := pool.Acquire()
+	defer pool.Release(conn)
+
+	if err != nil {
+		return err
+	}
+
+	db := conn.(*pg.DB)
+	err = db.Model(&c).Where(query, params).Select()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Credential) GetAll(pool *repository.Pool, ctx context.Context, query string, params ...string) ([]interface{}, error) {
+	conn, err := pool.Acquire()
+	defer pool.Release(conn)
+
+	if err != nil {
+		return []interface{}{}, err
+	}
+
+	var credentials []Credential
+
+	db := conn.(*pg.DB)
+	err = db.Model(&credentials).Where(query, params).Select()
+	if err != nil {
+		return []interface{}{}, err
+	}
+
+	var results = make([]interface{}, len(credentials))
+
+	for i := 0; i < len(credentials); i++ {
+		results[i] = credentials[i]
+	}
+
+	return results, nil
+}
+
+func (c *Credential) UpdateOne(pool *repository.Pool, ctx context.Context) error {
+	conn, err := pool.Acquire()
+	if err != nil {
+		return err
+	}
+	db := conn.(*pg.DB)
+	defer pool.Release(conn)
+
+	var credentialPlaceholder Credential
+	credentialPlaceholder.ID = c.ID
+
+	err = credentialPlaceholder.GetOne(pool, ctx, "id = ?", credentialPlaceholder.ID)
+
+	if credentialPlaceholder.ApiKey != c.ApiKey {
+		return errors.New("cannot update api key")
+	}
+
+	if err = db.Update(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Credential) DeleteOne(pool *repository.Pool, ctx context.Context) (int, error) {
+	conn, err := pool.Acquire()
+	if err != nil {
+		return -1, err
+	}
+	db := conn.(*pg.DB)
+	defer pool.Release(conn)
+
+	var credentials []Credential
+
+	err = db.Model(&credentials).Where("id = ?", c.ID).Select()
+	if err != nil {
+		return -1, err
+	}
+
+	if len(credentials) == 1 {
+		err = errors.New("cannot delete all the credentials")
+		return -1, err
+	}
+
+	r, err := db.Model(c).Where("id = ?", c.ID).Delete()
+	if err != nil {
+		return -1, err
+	}
+
+	return r.RowsAffected(), nil
+}
+
+func (c *Credential) SearchToQuery([][]string) (string, []string) {
+	return "", []string{""}
 }
 
 func (c *Credential) ToJson() []byte {
