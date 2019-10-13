3f4e7b6107141da14ec54b11919654fa0a069abd
Nwaokocha Victor <nvonweb@outlook.com>
2019-10-13 05:00:38 -0500

    Client should create redux store

--- client/src/renderers/dom.tsx
+++ client/src/renderers/dom.tsx
@@ -2,14 +2,18 @@
 import React from 'react'
 // @ts-ignore
 import ReactDOM from 'react-dom'
-import getStore from "../redux/store";
+import CreateStore from "../redux/store";
 import {Provider} from "react-redux";
 
 declare var window: {
     __INITIAL_DATA__: any,
 };
 
-export const clientRender = (Component: React.ElementType) => ReactDOM.hydrate(
-    <Provider store={getStore(window.__INITIAL_DATA__)}>
-        <Component />
-    </Provider>, document.getElementById('root'));
+export const clientRender = (Component: React.ElementType) => {
+    console.log(window.__INITIAL_DATA__);
+
+    return ReactDOM.hydrate(
+        <Provider store={CreateStore(window.__INITIAL_DATA__)}>
+            <Component/>
+        </Provider>, document.getElementById('root'));
+};
