51e96a83656a4434553fc12ed1489aa9041ae021
Nwaokocha Victor <nvonweb@outlook.com>
2019-10-13 03:19:05 -0500

    Add credential router post handler

--- client/src/routers/credential.ts
+++ client/src/routers/credential.ts
@@ -1,9 +1,20 @@
+// @ts-ignore
 import express from 'express';
 import axiosInstance from '../misc/axiosInstance'
 
 const router = express.Router();
 
 router.route("/")
+    .post((req, res) => {
+        console.log(req.body);
+        axiosInstance
+            .post('/credentials', req.body)
+            .then((resp) => {
+                res.status(resp.status).send(resp.data);
+            }).catch((e) => {
+                res.status(e.response.status).send(e.response.data);
+            });
+    })
     .get(async (_, res) => {
         let resp;
 
