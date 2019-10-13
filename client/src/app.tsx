967d0f40a5b7487b440907adf001d86880e2b4ab
Nwaokocha Victor <nvonweb@outlook.com>
2019-10-13 03:02:51 -0500

    Add drawer component to home page

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,35 +1,45 @@
-import React, { useEffect } from 'react'
+// @ts-ignore
+import React from 'react'
 import { hot } from 'react-hot-loader';
-import {createStyles, makeStyles, WithStyles, withStyles} from '@material-ui/core/styles';
+import {WithStyles, withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Store from "./models/store";
 import CredentialContainer from './containers/credential';
+import Drawer from '@material-ui/core/Drawer';
+import {List} from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-import theme from './theme';
-import {inject, observer, Provider} from "mobx-react";
-import {ThemeProvider} from "@material-ui/styles";
-import {CredentialState, ICredential, ICredentialModel} from "./models/credential";
+const drawerWidth = 240;
 
 const styles = theme => ({
     root: {
-      flexGrow: 1,
+        display: 'flex',
+        flexGrow: 1,
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
     },
     menuButton: {
       marginRight: theme.spacing(2),
     },
     mainContainer: {
-        paddingTop: '60px',
-        paddingLeft: '20px',
-        paddingRight: '20px'
-    }
+        flexGrow: 1,
+        paddingTop: '60px'
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
 });
 
 
-type Props = WithStyles<ReturnType<typeof styles>> & { rootStore: ICredentialModel }
+type Props = WithStyles<ReturnType<typeof styles>>
 
 class App extends React.Component<Props> {
     componentDidMount () {
@@ -39,27 +49,39 @@ class App extends React.Component<Props> {
 
     render() {
         const { classes } = this.props;
-
-        console.log(this.props);
-
         return (
             <section className={classes.root}>
-                <AppBar position="fixed">
+                <AppBar position="fixed" className={classes.appBar}>
                     <Toolbar variant="dense">
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
                         <Typography variant="h6" color="inherit">
                             Cron Server
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <section className={classes.mainContainer}>
-                    <CredentialContainer rootStore={this.props.rootStore} />
-                </section>
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{ paper: classes.drawerPaper }}
+                    anchor="left"
+                >
+                    <List>
+                        <ListItem button key={"API Keys"}>
+                            <ListItemText primary={"API Keys"} />
+                        </ListItem>
+                        <ListItem button key={"Projects"}>
+                            <ListItemText primary={"Projects"} />
+                        </ListItem>
+                        <ListItem button key={"Jobs"}>
+                            <ListItemText primary={"Jobs"} />
+                        </ListItem>
+                    </List>
+                </Drawer>
+                <main className={classes.mainContainer}>
+                    <CredentialContainer />
+                </main>
             </section>
         );
     }
 }
 
-export default hot(module)(withStyles(styles)(observer(App)));
+export default hot(module)(withStyles(styles)(App));
