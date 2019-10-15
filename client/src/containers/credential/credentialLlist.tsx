e4d6a3f80f0bacf0459e03e2faa1d6fb3e2fb986
Nwaokocha Victor <nvonweb@outlook.com>
2019-10-14 23:50:36 -0500

    Update and delete credential

--- client/src/containers/credential/credentialLlist.tsx
+++ client/src/containers/credential/credentialLlist.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import theme from '../../theme';
 import {ICredential} from "../../redux/credential";
 import CredentialListItem from "./creadentialListItem";
+import Box from "@material-ui/core/Box";
+import {Typography} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {FormMode} from "./index";
 
 interface IProps {
     credentials: ICredential[]
-    onDelete: (id) => Promise<void>
-    setCurrentCredentialId: (id: string) => () => void
+    setCurrentCredentialId: (id: string) => void
+    formMode: FormMode
+    deleteCredential: (id: string) => Promise<void>
+    setMode: (mode: FormMode) => () => void
 }
 
 const useStyles = makeStyles(theme => ({
@@ -15,7 +21,7 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(3, 2),
     },
     container: {
-        marginTop: '10px',
+        marginTop: '50px',
         padding: '20px',
     },
     header: {
@@ -27,24 +33,37 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-function Credential(props: IProps) {
+function CredentialList(props: IProps) {
     const classes = useStyles(theme);
-    const { credentials, onDelete, setCurrentCredentialId } = props;
+    const { credentials, deleteCredential, setCurrentCredentialId, setMode } = props;
+
+    const handleDelete = useCallback((credentialId) => {
+        deleteCredential(credentialId);
+    }, []);
 
     return (
-        <>
-            <div className={classes.container}>
-                {credentials && credentials.map((credential) => (
+        <div className={classes.container}>
+            <Box display="flex"
+                 component="div"
+                 flexDirection="row"
+                 alignItems="center"
+                 justifyContent="space-between"
+                 style={{ paddingLeft: '20px', paddingRight: '20px', marginBottom: '10px' }}>
+                <Typography variant="h5">Credentials</Typography>
+                <Button component="span" onClick={setMode(FormMode.Create)}>Create New Key</Button>
+            </Box>
+            <div>
+                {credentials && credentials.map((credential, index) => (
                     <CredentialListItem
-                        key={credential.api_key}
+                        key={`credential-${index}`}
                         credential={credential}
-                        onDelete={onDelete}
-                        setCurrentCredentialId={setCurrentCredentialId(credential.id)}
+                        onDelete={handleDelete}
+                        setCurrentCredentialId={() => setCurrentCredentialId(credential.id)}
                     />
                 ))}
             </div>
-        </>
+        </div>
     );
 }
 
-export default Credential;
+export default CredentialList;
--- client/src/containers/credential/modifyCredentialForm.tsx
+++ client/src/containers/credential/modifyCredentialForm.tsx
@@ -1,6 +1,6 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {connect} from "react-redux";
-import {CreateCredential, ICredential} from "../../redux/credential";
+import {CreateCredential, ICredential, UpdateCredential} from "../../redux/credential";
 import {makeStyles, Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -9,16 +9,23 @@ import {FormMode} from "./index";
 import Box from "@material-ui/core/Box";
 
 interface IProps {
-    mode?: FormMode
+    formMode?: FormMode
+    currentCredentialId?: string
     http_referrer_restriction?: string
+    setCurrentCredentialId?: (id: string) => void
+    setMode: (mode: FormMode) => () => void
+    currentCredential?: ICredential
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
     container: {
-        marginTop: '-10px',
+        marginTop: '50px',
         backgroundColor: '#f1f1f1',
-        height: 'calc(100vh - 110px)',
+        height: 'calc(100vh - 50px)',
         padding: '30px',
+        position: "sticky",
+        top: 50,
+        left: 0,
     },
     innerContainer: {
         marginTop: '20px',
@@ -28,43 +35,71 @@ const useStyles = makeStyles(theme => ({
 
 const CredentialsForm = (props: IProps & ReturnType<typeof mapDispatchToProps>) => {
     const classes = useStyles(theme);
-    const [error, setError] = useState("");
-    const { mode, http_referrer_restriction = "" } = props;
+    const [state, setState] = useState({ http_referrer_restriction: "" });
+    const { formMode, currentCredential = null, setMode, setCurrentCredentialId } = props;
+    const { http_referrer_restriction } = state;
 
-    const submit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    useEffect(() => {
+        setState({
+            http_referrer_restriction: currentCredential
+                ? currentCredential.http_referrer_restriction
+                : ""
+        });
+    }, [currentCredential]);
+
+    const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
         async function submitCredential(httpReferralUrl) {
             const credential: Partial<ICredential> = {};
             credential.http_referrer_restriction = httpReferralUrl;
+            const { formMode } = props;
+
             try {
-                await props.createCredential(credential);
-            } catch(err) {
-                setError(err.message);
+                if (formMode == FormMode.Create) {
+                    await props.createCredential(credential);
+                } else {
+                    await props.updateCredential(credential);
+                }
+            } catch (e) {
+                console.error(e);
             }
         }
+
         e.preventDefault();
-        const formValidity = e.currentTarget.checkValidity();
+
+        const form = e.currentTarget;
+        const formValidity = form.checkValidity();
         if (formValidity) {
             submitCredential(e.currentTarget.url.value);
+            setState({ http_referrer_restriction: "" });
         }
-    }, []);
+    }, [formMode]);
+    const cancelCallback = useCallback(() => {
+        if (formMode == FormMode.Edit) {
+            setCurrentCredentialId(null);
+        }
 
-    if (mode == FormMode.None) {
+        if (formMode == FormMode.Create) {
+            setMode(FormMode.None)();
+        }
+    }, [formMode]);
+
+    if (formMode == FormMode.None) {
         return null
     }
 
     return (
         <div className={classes.container}>
-            <Typography variant="h6">
-                New API Key
-            </Typography>
+            {(formMode == FormMode.Create) ?
+                <Typography variant="h6">New API Key</Typography>:
+                <Typography variant="h6">Edit API Key</Typography>}
             <div>
-                <form onSubmit={submit} name="new-credential">
-                    <p>{error}</p>
+                <form onSubmit={handleSubmit} name="new-credential">
                     <div className={classes.innerContainer}>
                         <TextField
                             fullWidth
                             required
-                            defaultValue={http_referrer_restriction}
+                            value={http_referrer_restriction}
+                            onChange={(e) => setState({ http_referrer_restriction: e.target.value })}
                             type="url"
                             name="url"
                             label="http referrer restriction"
@@ -73,7 +108,7 @@ const CredentialsForm = (props: IProps & ReturnType<typeof mapDispatchToProps>) 
                     <Box display="flex" flexDirection="row" justifyContent="flex-start" alignItems="center">
                         <Button type="submit" variant="contained">Submit</Button>
                         &nbsp;&nbsp;&nbsp;&nbsp;
-                        <Button component="span">Cancel</Button>
+                        <Button component="span" onClick={cancelCallback}>Cancel</Button>
                     </Box>
                 </form>
             </div>
@@ -81,11 +116,11 @@ const CredentialsForm = (props: IProps & ReturnType<typeof mapDispatchToProps>) 
     );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        createCredential: (credential: Partial<ICredential>) => dispatch(CreateCredential(credential))
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    createCredential: (credential: Partial<ICredential>) => dispatch(CreateCredential(credential)),
+    updateCredential: (credential: Partial<ICredential>) => dispatch(UpdateCredential(credential)),
+});
+
 const mapStateToProps = (state) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CredentialsForm);
