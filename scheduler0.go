d431c1fdaf944bc0d036c5249eec49233e00bd45
Nwaokocha Victor <nvonweb@outlook.com>
2021-02-23 03:32:30 -0500

    remove stale code

--- scheduler0.go
+++ scheduler0.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/spf13/viper"
-	"github.com/victorlenerd/scheduler0/server/src/utils"
-	"io/ioutil"
 	"os"
 	"scheduler0/cmd"
+	"scheduler0/server/src/utils"
 )
-
-func createConfigFile() {
-	config := cmd.Config{}
-	configByte, err := json.Marshal(config)
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-	err = ioutil.WriteFile(fmt.Sprintf("%v/.scheduler0", os.Getenv("HOME")), configByte, os.ModeAppend)
-	if err != nil {
-		panic(fmt.Errorf("Fatal unable to save scheduler 0: %s \n", err))
-	}
-}
 
 func setupViper() {
 	viper.SetConfigName("scheduler0")
@@ -28,21 +14,12 @@ func setupViper() {
 	viper.SetConfigFile(fmt.Sprintf("%v/.scheduler0", os.Getenv("HOME")))
 
 	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			createConfigFile()
-		} else {
-
-		}
-	} else {
-		fmt.Println(fmt.Sprintf("Hello %v", viper.Get("name")))
+		utils.Error(err.Error())
 	}
 }
 
 func main() {
 	setupViper()
-
-	utils.Info(fmt.Sprintf("Hello %v", viper.Get("Name")))
-
 	if err := cmd.Execute(); err != nil {
 		utils.Error(err.Error())
 	}
