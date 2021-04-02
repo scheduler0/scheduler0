c917a5e3059a99a5a6b5bab56987784150b0d725
Nwaokocha Victor <nvonweb@outlook.com>
2021-04-02 03:27:56 -0400

    add comment

--- utils/config.go
+++ utils/config.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 )
 
+// Scheduler0Configurations global configurations
 type Scheduler0Configurations struct {
 	PostgresAddress  string   `json:"postgres_address" yaml:"PostgresHost"`
 	PostgresUser     string   `json:"postgres_user" yaml:"PostgresUser"`
