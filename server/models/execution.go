b0da0a9a350b0d2187522cd827c25525d66f6761
Nwaokocha Victor <nvonweb@outlook.com>
2021-04-29 20:48:36 -0400

    add columns to help with recovery

--- server/models/execution.go
+++ server/models/execution.go
@@ -5,13 +5,15 @@ import "time"
 type ExecutionModel struct {
 	TableName struct{} `sql:"executions"`
 
-	ID          int64     `json:"id" sql:",pk:notnull"`
-	UUID        string    `json:"uuid" sql:",unique,type:uuid,default:gen_random_uuid()"`
-	JobID       int64     `json:"job_id" sql:",notnull"`
-	JobUUID     string    `json:"job_uuid" sql:",notnull,type:uuid"`
-	StatusCode  string    `json:"status_code"`
-	Timeout     uint64    `json:"timeout"`
-	DateCreated time.Time `json:"date_created" sql:",notnull,default:now()"`
+	ID            int64     `json:"id" sql:",pk:notnull"`
+	UUID          string    `json:"uuid" sql:",unique,type:uuid,default:gen_random_uuid()"`
+	JobID         int64     `json:"job_id" sql:",notnull"`
+	JobUUID       string    `json:"job_uuid" sql:",notnull,type:uuid"`
+	TimeAdded     time.Time `json:"time_added" sql:",notnull"`
+	TimeExecuted  time.Time `json:"time_executed"`
+	ExecutionTime uint64    `json:"execution_time"`
+	StatusCode    string    `json:"status_code"`
+	DateCreated   time.Time `json:"date_created" sql:",notnull,default:now()"`
 
 	Job JobModel `sql:",fk:job_id"`
 }
