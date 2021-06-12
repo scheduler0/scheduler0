3f2b0150b9c1fce27b77cea3ee884f77aac93cc2
Nwaokocha Victor <nvonweb@outlook.com>
2021-06-11 20:48:48 -0400

    add comments

--- server/service/project.go
+++ server/service/project.go
@@ -7,8 +7,11 @@ import (
 	"scheduler0/utils"
 )
 
+// ProjectService project server the layer on top db managers
 type ProjectService Service
 
+
+// CreateOne creates a new project
 func (projectService *ProjectService) CreateOne(project transformers.Project) (*transformers.Project, *utils.GenericError) {
 	projectManager := project.ToManager()
 
@@ -24,6 +27,7 @@ func (projectService *ProjectService) CreateOne(project transformers.Project) (*
 	return &projectTransformer, nil
 }
 
+// UpdateOne updates a single project
 func (projectService *ProjectService) UpdateOne(project transformers.Project) *utils.GenericError {
 	projectManager := project.ToManager()
 
@@ -39,6 +43,7 @@ func (projectService *ProjectService) UpdateOne(project transformers.Project) *u
 	return nil
 }
 
+// GetOneByUUID returns project with matching uuid
 func (projectService *ProjectService) GetOneByUUID(project transformers.Project) (*transformers.Project, *utils.GenericError) {
 	projectManager := project.ToManager()
 	err := projectManager.GetOneByUUID(projectService.Pool)
@@ -51,6 +56,8 @@ func (projectService *ProjectService) GetOneByUUID(project transformers.Project)
 	return &project, nil
 }
 
+
+// GetOneByName returns a project that matches the name
 func (projectService *ProjectService) GetOneByName(project transformers.Project) (*transformers.Project, *utils.GenericError) {
 	projectManager := project.ToManager()
 	err := projectManager.GetOneByName(projectService.Pool)
@@ -63,6 +70,7 @@ func (projectService *ProjectService) GetOneByName(project transformers.Project)
 	return &project, nil
 }
 
+// DeleteOne deletes a single project
 func (projectService *ProjectService) DeleteOne(project transformers.Project) *utils.GenericError {
 	projectManager := project.ToManager()
 	count, err := projectManager.DeleteOne(projectService.Pool)
@@ -77,6 +85,8 @@ func (projectService *ProjectService) DeleteOne(project transformers.Project) *u
 	return nil
 }
 
+
+// List return a paginated list of projects
 func (projectService *ProjectService) List(offset int, limit int) (*transformers.PaginatedProject, *utils.GenericError) {
 	project := transformers.Project{}
 	projectManager := project.ToManager()
