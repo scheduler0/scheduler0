8dbcf7365aeea0f324fd4a74e6c2439a1c4b748e
Nwaokocha Victor <nvonweb@outlook.com>
2022-10-25 23:53:56 -0400

    drop redundant constants

--- constants/constants.go
+++ constants/constants.go
@@ -9,10 +9,6 @@ const ConfigFileName = "config.yml"
 const ExecutionLogsDir = "logs"
 
 const (
-	COMMAND_TYPE_UNKNOWN     int32 = 0
-	COMMAND_TYPE_DB_QUERY    int32 = 1
-	COMMAND_TYPE_DB_EXECUTE  int32 = 2
-	COMMAND_TYPE_JOB_EXECUTE int32 = 3
-	COMMAND_TYPE_NOOP        int32 = 4
-	COMMAND_TYPE_LOAD        int32 = 5
+	CommandTypeDbExecute int32 = 0
+	CommandTypeJobQueue  int32 = 1
 )
