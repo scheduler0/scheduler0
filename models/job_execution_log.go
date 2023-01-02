6b50c3461c8479f174e83949e0b58740854129a6
Nwaokocha Victor <nvonweb@outlook.com>
2023-01-02 03:53:34 -0500

    job execution log table

---
+++ models/job_execution_log.go
@@ -0,0 +1,36 @@
+package models
+
+import "time"
+
+type JobExecutionLogState uint64
+
+const (
+	ExecutionLogScheduleState JobExecutionLogState = 0
+	ExecutionLogSuccessState                       = 1
+	ExecutionLogFailedState                        = 2
+)
+
+type JobExecutionLog struct {
+	Id                    uint64    `json:"id"`
+	UniqueId              string    `json:"uniqueId"`
+	State                 uint64    `json:"state"`
+	NodeId                uint64    `json:"nodeId"`
+	LastExecutionDatetime time.Time `json:"lastExecutionDatetime"`
+	NextExecutionDatetime time.Time `json:"nextExecutionDatetime"`
+	JobId                 int64     `json:"jobId"`
+	JobQueueVersion       uint64    `json:"job_queue_version"`
+	ExecutionVersion      uint64    `json:"execution_version"`
+	DataCreated           time.Time `json:"dataCreated"`
+}
+
+type CommitJobStateLog struct {
+	Address string            `json:"address"`
+	Logs    []JobExecutionLog `json:"logs"`
+}
+
+type MemJobExecution struct {
+	FailCount             uint64
+	LastState             JobExecutionLogState
+	LastExecutionDatetime time.Time `json:"lastExecutionDatetime"`
+	NextExecutionDatetime time.Time `json:"nextExecutionDatetime"`
+}
