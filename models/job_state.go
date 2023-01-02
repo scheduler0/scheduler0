afd7f7f7022a6f716f4ed6cc566e109e28df3f32
Nwaokocha Victor <nvonweb@outlook.com>
2023-01-02 04:18:32 -0500

    adds node ide to job state log struct

--- models/job_state.go
+++ models/job_state.go
@@ -1,13 +1,13 @@
 package models
 
 import (
-	"scheduler0/constants"
 	"time"
 )
 
 type JobStateLog struct {
-	ExecutionTime time.Time         `json:"executionTime,omitempty"`
-	ServerAddress string            `json:"server_address,omitempty"`
-	State         constants.Command `json:"state,omitempty"`
-	Data          []JobModel        `json:"data,omitempty"`
+	ExecutionTime     time.Time            `json:"executionTime,omitempty"`
+	NodeId            uint64               `json:"node_id,omitempty"`
+	ServerHTTPAddress string               `json:"serverHTTPAddress"`
+	State             JobExecutionLogState `json:"state,omitempty"`
+	Data              []JobModel           `json:"data,omitempty"`
 }
