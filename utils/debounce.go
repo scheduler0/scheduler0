e7d9a65914dcd4eaff843b5a125c08de3cf01fb7
Nwaokocha Victor <nvonweb@outlook.com>
2023-01-11 23:47:42 -0500

    use mutex for in debouncer

--- utils/debounce.go
+++ utils/debounce.go
@@ -6,33 +6,42 @@ import (
 	"time"
 )
 
-type debounce struct {
-	ticker    time.Ticker
+type Debounce struct {
+	ticker    *time.Ticker
 	once      sync.Once
 	threshold time.Time
+	mtx       sync.Mutex
 }
 
-type Debounce interface {
-	Debounce(ctx context.Context, delay int64, effector func())
-}
-
-func NewDebounce() *debounce {
-	return &debounce{
+func NewDebounce() *Debounce {
+	return &Debounce{
 		once: sync.Once{},
 	}
 }
 
-func (d *debounce) Debounce(ctx context.Context, delay int64, effector func()) {
+func (d *Debounce) Debounce(ctx context.Context, delay int64, effector func()) {
+	d.mtx.Lock()
+	defer d.mtx.Unlock()
+
 	d.threshold = time.Now().Add(time.Duration(delay) * time.Millisecond)
 	d.once.Do(func() {
 		go func() {
+			defer func() {
+				d.mtx.Lock()
+				defer d.mtx.Unlock()
+
+				d.ticker.Stop()
+				d.once = sync.Once{}
+			}()
+
+			d.ticker = time.NewTicker(time.Duration(500) * time.Millisecond)
+
 			for {
 				select {
 				case <-d.ticker.C:
 					if time.Now().After(d.threshold) {
 						effector()
-						d.ticker.Stop()
-						d.once = sync.Once{}
+						return
 					}
 				case <-ctx.Done():
 					return
