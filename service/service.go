f8524760d4c8b371383fb294df4b619f1f48dc04
Nwaokocha Victor <nvonweb@outlook.com>
2023-01-19 03:01:30 -0500

    adds service method

---
+++ service/service.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"context"
+	"log"
+	"scheduler0/config"
+	"scheduler0/db"
+	"scheduler0/fsm"
+	"scheduler0/repository"
+	"scheduler0/service/executor"
+	"scheduler0/service/node"
+	"scheduler0/service/queue"
+	"scheduler0/utils"
+	"scheduler0/utils/workers"
+)
+
+type Service struct {
+	Dispatcher         *workers.Dispatcher
+	JobService         Job
+	ProjectService     Project
+	CredentialService  Credential
+	JobExecutorService *executor.JobExecutor
+	NodeService        *node.Node
+	JobQueueService    *queue.JobQueue
+}
+
+func NewService(ctx context.Context, logger *log.Logger) *Service {
+	configs := config.GetConfigurations(logger)
+
+	schedulerTime := utils.GetSchedulerTime()
+	err := schedulerTime.SetTimezone("UTC")
+	if err != nil {
+		logger.Fatalln("failed to set timezone for s")
+	}
+	sqliteDb := db.GetDBConnection(logger)
+	fsmStr := fsm.NewFSMStore(sqliteDb, logger)
+
+	//repository
+	credentialRepo := repository.NewCredentialRepo(logger, fsmStr)
+	jobRepo := repository.NewJobRepo(logger, fsmStr)
+	projectRepo := repository.NewProjectRepo(logger, fsmStr, jobRepo)
+	executionsRepo := repository.NewExecutionsRepo(logger, fsmStr)
+	jobQueueRepo := repository.NewJobQueuesRepo(logger, fsmStr)
+
+	jobExecutor := executor.NewJobExecutor(ctx, logger, jobRepo, executionsRepo, jobQueueRepo)
+	jobQueue := queue.NewJobQueue(ctx, logger, fsmStr, jobExecutor, jobQueueRepo)
+	nodeService := node.NewNode(ctx, logger, jobExecutor, jobQueue, jobRepo, projectRepo, executionsRepo, jobQueueRepo)
+
+	nodeService.FsmStore = fsmStr
+
+	service := Service{
+		JobService:         NewJobService(ctx, logger, jobRepo, jobQueue, projectRepo),
+		ProjectService:     NewProjectService(logger, projectRepo),
+		CredentialService:  NewCredentialService(ctx, logger, credentialRepo),
+		JobExecutorService: jobExecutor,
+		NodeService:        nodeService,
+		JobQueueService:    jobQueue,
+	}
+
+	service.Dispatcher = workers.NewDispatcher(
+		int64(configs.IncomingRequestMaxWorkers),
+		int64(configs.IncomingRequestMaxQueue),
+		func(args ...any) {
+			//requests := args[0].([]models.ServiceRequest)
+			// TODO: Pass request to appropriate service
+		},
+	)
+
+	service.Dispatcher.Run()
+
+	return &service
+}
