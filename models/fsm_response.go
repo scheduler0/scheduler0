e4fadf1a941d41b1e358ee35164c3ab009880749
Nwaokocha Victor <nvonweb@outlook.com>
2024-07-29 17:25:55 -0400

    improved command labels and response struct

--- models/fsm_response.go
+++ models/fsm_response.go
@@ -1,6 +1,11 @@
 package models
 
-type Response struct {
-	Data  []interface{} `json:"data"`
-	Error string        `json:"error"`
+type SQLResponse struct {
+	RowsAffected   int64 `json:"rowsAffected"`
+	LastInsertedId int64 `json:"LastInsertedId"`
+}
+
+type FSMResponse struct {
+	Data  SQLResponse `json:"data"`
+	Error string      `json:"error"`
 }
